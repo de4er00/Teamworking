85f70e1664a5791eda50c12ba7b2780f8477602a
pyatkin2000 <45499854+pyatkin2000@users.noreply.github.com>
2018-12-15 16:46:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "func.h"
+#include "file.h"
 
-pslist list_new(void)
+int main(void)
+{
+    pslist spisok;
+
+    spisok = slist_new();
+
+    insert_end(spisok, 5);
+    insert_end(spisok, 5);
+    insert_end(spisok, 5);
+    insert_end(spisok, 1);
+    insert_end(spisok, 1);
+    insert_end(spisok, 5);
+    insert_end(spisok, 5);
+    insert_end(spisok, 5);
+    insert_end(spisok, 1);
+    insert_end(spisok, 2);
+    insert_end(spisok, 5);
+    insert_end(spisok, 5);
+    insert_end(spisok, 1);
+    insert_end(spisok, 5);
+    insert_end(spisok, 3);
+    insert_end(spisok, 5);
+    insert_end(spisok, 1);
+    insert_end(spisok, 5);
+
+    print(spisok);
+    slist_remove(spisok, 5);
+    print(spisok);
+    slist_remove(spisok, 1);
+    print(spisok);
+    slist_remove(spisok, 2);
+    print(spisok);
+    slist_remove(spisok, 3);
+    print(spisok);
+    printf("-----");
+    slist_delete(spisok);
+
+
+    spisok = slist_new();
+
+    insert_start(spisok, 5);
+    insert_start(spisok, 5);
+    insert_start(spisok, 5);
+    insert_start(spisok, 1);
+    insert_start(spisok, 1);
+    insert_start(spisok, 5);
+    insert_start(spisok, 5);
+    insert_start(spisok, 5);
+    insert_start(spisok, 1);
+    insert_start(spisok, 2);
+    insert_start(spisok, 5);
+    insert_start(spisok, 5);
+    insert_start(spisok, 1);
+    insert_start(spisok, 5);
+    insert_start(spisok, 3);
+    insert_start(spisok, 5);
+    insert_start(spisok, 1);
+    insert_start(spisok, 5);
+
+    print(spisok);
+    slist_remove(spisok, 5);
+    print(spisok);
+    slist_remove(spisok, 1);
+    print(spisok);
+    slist_remove(spisok, 2);
+    print(spisok);
+    slist_remove(spisok, 3);
+    print(spisok);
+    printf("-----");
+    slist_delete(spisok);
+
+
+    return 0;
+}
+
+pslist slist_new(void)//создание нового листа
 {
 
     pslist list = malloc(sizeof(struct slist)); // объявление указателя, выделение памяти
@@ -16,33 +91,50 @@ pslist list_new(void)
     return list; //возвращаем указатель на новый список (указатель на структуру slist)
 }
 
-/*
-pslist_entry slist_pop(pslist list) // переносит head на следующий элемент списка и возвращает старое значение head
+void slist_delete(pslist list)//удаление листа по указателю на лист
 {
-    if (NULL != list->head)
+    pslist_entry head2 = malloc(sizeof(struct slist_entry));
+
+    while(NULL != list->head)//можно через list_size
     {
-        return list->head; // возвращает значение head на данный момент
-        list->head = (list->head)->next; // переносит head на следующий элемент списка
-    }
-    else return NULL;
-}
-
-
-void list_delete(pslist list)
-{
-    pslist_entry head; //указатель на первый элемент списка
-
-    while(NULL != (head = slist_pop(list)))
-    {
-        free(head); // удаляем каждый элемент списка (освобождаем пямять)
+        head2 = list->head;
+        list->head = list->head->next;
+        free(head2); // удаляем каждый элемент списка (освобождаем пямять)
     }
 
     free(list); // после удаления всех элементв удаляем указатель на список
 }
-*/
-int insert(pslist list, int value)
+
+
+int insert_end(pslist list, int value)//вставка ового элемента в конец списка
 {
-    struct slist_entry *pnew = malloc(sizeof(struct slist_entry));
+    pslist_entry pnew = malloc(sizeof(struct slist_entry));//  объявление указателя на новый элемент, выделение памяти
+    pslist_entry pend = list->head;
+
+
+    if (pnew == NULL)
+        return -1;  //?
+
+
+    if (NULL == list->head) list->head = pnew;
+    else {
+        while(NULL != pend->next) pend = pend->next;
+        pend->next = pnew;
+    }
+
+
+    list->list_size++;
+    pnew->next = NULL;
+    pnew->value = value;
+
+    printf("New element: %d\n", pnew->value);
+
+    return 0;
+}
+
+int insert_start(pslist list, int value)//вставка нового элемента в начало списка
+{
+    struct slist_entry *pnew = malloc(sizeof(struct slist_entry)); //  объявление указателя на новый элемент, выделение памяти
   //struct pslist_entry pnew = malloc(sizeof(struct slist_entry)); ?
 
     if (pnew == NULL)
@@ -60,47 +152,67 @@ int insert(pslist list, int value)
     return 0;
 }
 
-/*
-int list_remove(pslist list, int value)
+
+int slist_remove(pslist list, int value)//удаление всех элементов со значением value
 {
+    //pslist_entry pold, pnew ,pend = malloc(sizeof(struct slist_entry));?
+    pslist_entry pold = malloc(sizeof(struct slist_entry));
+    pslist_entry pend = malloc(sizeof(struct slist_entry));
+    pslist_entry pnew = malloc(sizeof(struct slist_entry));
+
+
+    while((NULL != list->head)&&(list->head->value == value))
+    {
+        pold = list->head;
+        list->head = list->head->next;
+        free(pold);  // удаление структуры, на которую указывает указатель??? как выглядит удаление самой переменной pold???
+    }
+
+    pend = list->head;
+
+    while(NULL != pend)
+    {
+        if (pend->value == value)
+        {
+
+            while((NULL != pend->next)&&(pend->value == value))
+            {
+                pold = pend;
+                pend = pend->next;
+
+                free(pold);
+
+                list->list_size--;
+            }
+
+            if (pend->value != value) pnew->next = pend;
+            else pnew->next = NULL;
+        }
+
+        pnew = pend;
+        pend = pend->next;
+
+    }
+    //free(&pold); //освобождение выделенной памяти под указатель?? нужно ли?
+    //free(*pend);
+    pold = pend;
     return 0;
 }
-*/
 
-void print(pslist list)
+
+
+void print(pslist list)//выводит элементы списка по порядку начиная с первого
 {
-    if ((NULL != list->head)&&(NULL != list))
-    {
-    pslist_entry pcurrent = list->head;
+    pslist_entry pend = list->head;
 
     printf("List: \n");
 
-    while (pcurrent != NULL)
+    while (pend != NULL)
     {
-        printf("> %d\n", pcurrent->value);
+        printf("> %d\n", pend->value);
 
-        pcurrent = pcurrent->next;
+        pend = pend->next;
     }
-    }
-    else
-    if (NULL == list->head) printf("Список пуст");
-    else
-    printf("Списка нет");
+
+    printf("end\n");
 }
-
-
-int main(void)
-{
-    pslist spisok;
-
-
-    spisok = list_new();
-
-    insert(spisok, 1);
-    insert(spisok, 2);
-
-    print(spisok);
-
-    return 0;
-}
-
